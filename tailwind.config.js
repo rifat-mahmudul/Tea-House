960720c5f5f9e693af92bd24b6350f27128f4993
Mahmudul Hasan Rifat <mahmudulhasanrifat201@gmail.com>
2024-08-18 10:36:13 +0600

    header section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "color-primary" : "#01051e",
+        "color-primary-light": "#020726",
+        "color-primary-dark": "#010417",
+        "color-secoundary": "#ff7d3b",
+        "color-blob": "#A427DF",
+      },
+
+      fontFamily:{
+        "manrope" : ["Manrope", "sans-serif"],
+        "Gray-Qo" : ["Grey Qo", "cursive"],
+      }
+    },
   },
   plugins: [],
 }
